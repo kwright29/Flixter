df4b0269e8111319a51a105761f91c183dca1ab4
kwright29 <khloewright329@gmail.com>
2022-06-15 14:40:57 -0700

    step 2 pt 1- movieViewController

---
+++ Flixter/MovieViewController.h
@@ -0,0 +1,14 @@
+//
+//  MovieViewController.h
+//  Flixter
+//
+//  Created by Khloe Wright on 6/15/22.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface MovieViewController : UIViewController
+
+
+@end
