d10195f554b6ae6cf22dcfc05ad337891ac5d532
kwright29 <khloewright329@gmail.com>
2022-06-22 13:24:15 -0700

    added MovieAPI

---
+++ Flixter/MovieApiManager.h
@@ -0,0 +1,16 @@
+//
+//  MovieApiManager.h
+//  Flixter
+//
+//  Created by Khloe Wright on 6/22/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieApiManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
